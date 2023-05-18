889cc0650f36eb719ad398089e9d2f28266aa4c8
jasonaruiz95 <jasonruiz1@mail.weber.edu>
2023-05-18 00:00:23 -0600

    Added Delete page and backend functionality

---
+++ CBTDWeb/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,53 @@
+using CBTDWeb.Data;
+using CBTDWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CBTDWeb.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Category objCategory { get; set; }
+
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult OnGet(int? id)
+        {
+            objCategory = new Category();
+
+
+            objCategory = _db.Categories.Find(id);
+
+            if (objCategory == null)
+            {
+                return NotFound();
+            }
+
+            //create new mode
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+
+            _db.Categories.Remove(objCategory);
+            TempData["success"] = "Category removed Successfully";
+
+            _db.SaveChanges();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
+
